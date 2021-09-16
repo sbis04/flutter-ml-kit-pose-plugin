7e79852aba597a46ec85079559a7151652082f46
Souvik Biswas <sbis1999@gmail.com>
2021-09-16 18:47:53 +0530

    Sets the pose name and accuracy to null and 0.0 resp

--- android/src/main/java/com/google_ml_kit/vision/PoseDetector.java
+++ android/src/main/java/com/google_ml_kit/vision/PoseDetector.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -181,5 +182,6 @@ public class PoseDetector implements ApiDetectorInterface {
 
     private void closeDetector() {
         poseDetector.close();
+        PoseDataStorage.setData(null, 0.0);
     }
 }
