ddfc3c404ed245a85a65f0cfd171856722df10a9
Souvik Biswas <sbis1999@gmail.com>
2021-12-05 11:25:31 +0530

    Update pose processor to track reps of two activity

--- android/src/main/java/com/google_ml_kit/vision/classification/PoseClassifierProcessor.java
+++ android/src/main/java/com/google_ml_kit/vision/classification/PoseClassifierProcessor.java
@@ -23,19 +23,35 @@ import java.util.Locale;
 public class PoseClassifierProcessor {
     private static final String TAG = "PoseClassifierProcessor";
     private static final String POSE_SAMPLES_FILE = "pose/fitness_poses_csvs_out_all.csv";
+    private static final String POSE_ACTIVITY = "pose/fitness_pose_activities.csv";
     // private static final String POSE_SAMPLES_FILE = "pose/fitness_poses_csvs_out_without_outliers.csv";
     // private static final String POSE_SAMPLES_FILE = "pose/fitness_poses_csvs_out_with_outliers.csv";
     //  private static final String POSE_SAMPLES_FILE = "pose/beginners_poses_csvs_out.csv";
 
+    private static final String PUSHUPS_CLASS = "pushups_down";
+    private static final String SQUATS_CLASS = "squats_down";
+    private static final String[] POSE_CLASSES = {
+            PUSHUPS_CLASS, SQUATS_CLASS
+    };
+
     final private EMASmoothing emaSmoothing;
+    private ArrayList<RepetitionCounter> repCounters;
     private PoseClassifier poseClassifier;
+    private String currentActivityClass;
 
     @WorkerThread
-    public PoseClassifierProcessor(Context context) {
+    public PoseClassifierProcessor(Context context, boolean isActivity) {
         Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
         emaSmoothing = new EMASmoothing();
 
-        loadPoseSamples(context);
+        if (isActivity) {
+            repCounters = new ArrayList<>();
+            loadPoseSamplesWithActivity(context);
+        } else {
+            loadPoseSamples(context);
+        }
+
+
     }
 
     private void loadPoseSamples(Context context) {
@@ -58,6 +74,32 @@ public class PoseClassifierProcessor {
         poseClassifier = new PoseClassifier(poseSamples);
     }
 
+    private void loadPoseSamplesWithActivity(Context context) {
+        List<PoseSample> poseSamples = new ArrayList<>();
+        RepetitionCounter.numRepeats = 0;
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open(POSE_ACTIVITY)));
+            String csvLine = reader.readLine();
+            while (csvLine != null) {
+                // If line is not a valid {@link PoseSample}, we'll get null and skip adding to the list.
+                PoseSample poseSample = PoseSample.getPoseSample(csvLine, ",");
+                if (poseSample != null) {
+                    poseSamples.add(poseSample);
+                }
+                csvLine = reader.readLine();
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error when loading pose samples.\n" + e);
+        }
+        poseClassifier = new PoseClassifier(poseSamples);
+
+        for (String className : POSE_CLASSES) {
+            repCounters.add(new RepetitionCounter(className));
+        }
+
+    }
+
     /**
      * Given a new {@link Pose} input, returns a list of formatted {@link String}s with Pose
      * classification results.
@@ -75,6 +117,53 @@ public class PoseClassifierProcessor {
 
         if (pose.getAllPoseLandmarks().isEmpty()) {
             return result;
+        }
+
+        // Add maxConfidence class of current frame to result if pose is found.
+        if (!pose.getAllPoseLandmarks().isEmpty()) {
+            String maxConfidenceClass = classification.getMaxConfidenceClass();
+            float poseAccuracy = classification.getClassConfidence(maxConfidenceClass) / poseClassifier.confidenceRange();
+
+            // THESE ARE REQUIRED IN FLUTTER
+            // -----------------------------------------------------------------------------------
+            // Log.i("POSE DETECTED: ", maxConfidenceClass);
+            // Log.i("POSE ACCURACY: ", String.format(Locale.US, "%.2f", poseAccuracy));
+            // -----------------------------------------------------------------------------------
+            //
+            PoseDataStorage.setData(maxConfidenceClass, Double.parseDouble(String.format(Locale.US, "%.2f", poseAccuracy)));
+        }
+
+        return result;
+    }
+
+    @WorkerThread
+    public List<String> getPoseResultWithReps(Pose pose) {
+        Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
+        List<String> result = new ArrayList<>();
+
+        ClassificationResult classification = poseClassifier.classify(pose);
+        classification = emaSmoothing.getSmoothedResult(classification);
+
+        if (pose.getAllPoseLandmarks().isEmpty()) {
+            return result;
+        }
+
+        for (RepetitionCounter repCounter : repCounters) {
+            int repsBefore = repCounter.getNumRepeats();
+
+            if (repsBefore <= 1) {
+                repCounter.addClassificationResult(classification);
+                currentActivityClass = repCounter.getClassName();
+            } else {
+                if (repCounter.getClassName().equals(currentActivityClass)) {
+                    int repsAfter = repCounter.addClassificationResult(classification);
+                    if (repsAfter > repsBefore) {
+                        break;
+                    }
+                }
+            }
+
+
         }
 
         // Add maxConfidence class of current frame to result if pose is found.
